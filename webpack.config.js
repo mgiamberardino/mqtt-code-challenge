6c97747cf084d4ff27432e8d468b09dc7f70d547
Mauro Giamberardino <mgiamberardino@gmail.com>
2019-05-24 16:05:36 -0300

    React app for challenge using a webpack config from scratch

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        app: "./src/app/index.tsx",
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /(node_modules|bower_components|dist)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"]},
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "[name].bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.EnvironmentPlugin(['MQTT_HOST']),
+    ],
+};
